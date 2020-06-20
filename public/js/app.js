2fd1568e006f909cdc023c817d61aa7f0eca51b3
Duke Yoo <dukuaris@gmail.com>
2020-06-20 15:15:48 +0900

    Remove unneccessay console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded');
-
 const weatherForm = document.querySelector('form');
 const searchEl = document.querySelector('input');
 const messageOne = document.getElementById('message-1');
